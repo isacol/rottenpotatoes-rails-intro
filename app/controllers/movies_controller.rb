d5e82d66be0d2474f1f67903a3a70695fa1bf4b5
Isabela Colmenar <isabelac@berkeley.edu>
2022-10-09 16:24:33 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,14 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      #@movies = Movie.all
+      @all_ratings = Movie.all_ratings
+      @ratings_to_show = []
+      if params[:ratings].nil? == false
+        @ratings_to_show = params[:ratings].keys()
+      end
+      @movies = Movie.with_ratings(@ratings_to_show)
+      #redirect_to movies_path
     end
   
     def new

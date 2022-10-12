6bfb3efaf3d51bed62f43358878477758eb0d843
Isabela Colmenar <isabelac@berkeley.edu>
2022-10-12 01:30:57 +0000

    try part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,14 +6,58 @@ class MoviesController < ApplicationController
       # will render app/views/movies/show.<extension> by default
     end
   
+    #only save param if id is what you want it to be - if filter id is = to ___ , save param in session
+
+    #case 1: nothing in param or session
+    #  run the defaults - all ratings, and no filter
+    #case 2: something in param PERIOD --> run with that param, reset session
+    #case 3: something in session thats not in param 
+
+    # if exsits sort/filter in params or session 
+
+    #(something in the session, something in the param, nothing)
     def index
       #@movies = Movie.all
       @all_ratings = Movie.all_ratings
       @ratings_to_show = []
-      if params[:ratings].nil? == false
+
+      # if params[:ratings].nil? and params[:sort].nil? # if new call doesnt have params sort or ratings
+      #   if session[:ratings].nil?
+      #     ratings = session[:ratings]
+      #     redirect_to movies_path(:ratings => ratings)
+      #   elsif session[:sort].nil?
+      #     sort = session[:sort]
+      #     redirect_to movies_path(:sort => sort)
+      # end
+
+      if !params.has_key?(:ratings) and !params.has_key(:sort):
+        if session.has_key(:ratings)
+          ratings = session[:ratings]
+        end
+        if
+      elsif params.has_key?(:ratings) and !params.has_key?(:sort)
+        session[:ratings] = params[:ratings]
+        if session.key?(:sort)
+          sort = session[:ratings]
+          ratings = params[:ratings]
+          redirect_to movies_path(:sort => sort, :ratings => ratings)
+        end
+      elsif params.has_key?(:sort) and !params.has_key(:ratings)
+        session[:sort] = params[:sort]
+        if session.key?(:ratings)
+          ratings = session[:ratings]
+          sort = params[:sort]
+          redirect_to movies_path(:sort => sort, :ratings => ratings)
+        end
+      end
+
+
+      #BELOW = PURE CODE. dont touch - ensure cases before this hit
+      if params.has_key?(:ratings)
         @ratings_to_show = params[:ratings].keys()
         @ratings_to_show_hash = Hash[@ratings_to_show.map {|k| [k, '1']}]
       end
+      
       @movies = Movie.with_ratings(@ratings_to_show)
 
       @title_header = ''
@@ -27,11 +71,6 @@ class MoviesController < ApplicationController
         if params[:sort] == 'release_date'
           @release_date_bg = 'hilite bg-warning'
         end
-      end
-
-      if params[:ratings].nil? == false
-        v = params[:ratings].keys
-        @movies = @movies.with_ratings(v)
       end
 
     end
